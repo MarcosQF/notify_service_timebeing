81e1a8a66d20f439bea5992b12bcc4a138a9f564
Marcos Queiroz <marcosamfq123@gmail.com>
2025-08-04 18:17:55 -0300

    correção do connect no websocket

--- notify_service/ws_manager.py
+++ notify_service/ws_manager.py
@@ -8,11 +8,11 @@ class ConnectionManager:
     async def connect(self, user_id: str, websocket: WebSocket):
         await websocket.accept()
         self.active_connections[user_id] = websocket
+        await websocket.send_text("connected")
         print(
             f"✅ User {user_id} connected. Total connections: \
             {len(self.active_connections)}"
         )
-
     def disconnect(self, user_id: str):
         self.active_connections.pop(user_id, None)
         print(
